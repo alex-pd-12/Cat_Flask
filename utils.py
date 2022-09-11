ac3ad03c06cc48c30dfc03787015650182250d9b
Alexey <leshabazhenov@vk.com>
2022-09-12 02:50:08 +0300

    home_work_11

---
+++ utils.py
@@ -0,0 +1,36 @@
+import json
+
+
+def load_candidates_from_json() -> list[dict]:
+    """Возвращает список всех кандидатов"""
+    with open("candidates.json", "r", encoding="UTF-8") as file:
+        data = json.load(file)
+        return data
+
+
+def get_candidate(candidate_id: int) -> dict:
+    """Возвращает одного кандидата по его id"""
+    for candidate in load_candidates_from_json():
+        if candidate["id"] == candidate_id:
+            return candidate
+
+
+def get_candidates_by_name(candidate_name: str) -> list[dict]:
+    """Возвращает кандидатов по имени"""
+    result = []
+    for candidate in load_candidates_from_json():
+        if candidate["name"] == candidate_name:
+            result.append(candidate)
+
+    return result
+
+
+def get_candidates_by_skill(skill_name: str) -> list[dict]:
+    """Возвращает кандидатов по навыку"""
+    result = []
+    for candidate in load_candidates_from_json():
+        if skill_name in candidate["skills"].lower().split(", "):
+            result.append(candidate)
+
+    return result
+
